0066eb246860537e566861b8d174006b2452e3f7
Marley Stipich <Marley.Stipich@charity.org>
2021-07-12 10:23:02 -0500

    Updates

---
+++ precache-manifest.502004f1b32e3ae89826043f0ec87997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ce41e4b8dd4d9e1dbd9",
+    "url": "/static/css/main.0cc2f2a3.chunk.css"
+  },
+  {
+    "revision": "5ce41e4b8dd4d9e1dbd9",
+    "url": "/static/js/main.5ce41e4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "fb01f485cd1ba13564f1",
+    "url": "/static/js/2.fb01f485.chunk.js"
+  },
+  {
+    "revision": "5c7591de076216f613b88ec8a803c323",
+    "url": "/index.html"
+  }
+];
