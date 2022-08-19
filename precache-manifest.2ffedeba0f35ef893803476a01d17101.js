e96e58896e79f26839a0108c0d12a5ecefc5ab01
Marley Stipich <mstipich@redhat.com>
2022-08-18 20:11:27 -0500

    Updates

---
+++ precache-manifest.2ffedeba0f35ef893803476a01d17101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f4f6b793e315c2fb026",
+    "url": "/static/css/main.68212298.chunk.css"
+  },
+  {
+    "revision": "6f4f6b793e315c2fb026",
+    "url": "/static/js/main.6f4f6b79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "fb01f485cd1ba13564f1",
+    "url": "/static/js/2.fb01f485.chunk.js"
+  },
+  {
+    "revision": "ae071fd96b4f19205ada7fe21185262d",
+    "url": "/index.html"
+  }
+];
